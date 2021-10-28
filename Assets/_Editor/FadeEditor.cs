d7c3dbfc87949675ea937c926e4418b904102a20
Anna Chen <iloveelmo1107@gmail.com>
2021-10-28 17:11:53 -0600

    Add Fade Editor

---
+++ Assets/_Editor/FadeEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections;
+[CustomEditor(typeof(FadeColor))]
+
+public class FadeEditor : Editor
+{
+    private void OnEnable()
+    {
+        
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("fadingSpeed"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("interpolationFramesCount"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("ColorList"), true);
+        //EditorList.Show(serializedObject.FindProperty("ColorList"));
+        serializedObject.ApplyModifiedProperties();
+
+
+    }
+
+}
--- Assets/_Scripts/FadeColor.cs
+++ Assets/_Scripts/FadeColor.cs
@@ -8,11 +8,12 @@ public class FadeColor : MonoBehaviour
     Color originColor;
     Material _material;
     
-    [SerializeField]
-    float fadingSpeed = 0.05f;
+    //[SerializeField]
+    public float fadingSpeed = 0.05f;
 
-    [SerializeField]
-    Color targetColor;
+    //[SerializeField]
+    //Color targetColor;
+    public Color[] ColorList;
 
     //public Material _material;
     public int interpolationFramesCount = 45; // Number of frames to completely interpolate between the 2 positions
@@ -38,12 +39,32 @@ public class FadeColor : MonoBehaviour
     {
 
         float interpolationRatio;
+        /*
         while ( _material.color != targetColor)
         {
             interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
             _material.SetColor("_Color" ,Color.Lerp(originColor, targetColor, interpolationRatio));
             elapsedFrames = (elapsedFrames + 1) % (interpolationFramesCount + 1);
             yield return new WaitForSeconds(fadingSpeed);
+        }
+        */
+        interpolationFramesCount = interpolationFramesCount / ColorList.Length;
+
+        for(int i = 0; i < ColorList.Length; i++)
+        {
+            if (i != 0)
+            {
+                originColor = ColorList[i - 1];
+            }
+            Color targetColor = ColorList[i];
+
+            while (_material.color != targetColor)
+            {
+                interpolationRatio = (float)elapsedFrames / interpolationFramesCount;
+                _material.SetColor("_Color", Color.Lerp(originColor, targetColor, interpolationRatio));
+                elapsedFrames = (elapsedFrames + 1) % (interpolationFramesCount + 1);
+                yield return new WaitForSeconds(fadingSpeed);
+            }
         }
     }
 
