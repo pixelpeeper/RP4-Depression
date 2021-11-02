2d6c02639cf1b2635bedad45d0e14b32d0bc3268
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2021-11-02 14:56:36 -0600

    NPC dialogue almost done

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,11 +6,15 @@ public class GameManager : MonoBehaviour
 {
     string[] SceneOne = { "Other visual scripting solutions use delegates or reflection calls for every single node in the graph. Even simple behaviors need many nodes. This quickly adds up to a heavy hit on performance!Even simple behaviors need many nodes.", "next", "end" };
     string[] SceneTwo = { "Activates/Deactivates the GameObject, depending on the given true or false value. ", "next...", "Deactivating a GameObject disables each component, including attached renderers, colliders, rigidbodies, and scripts. " };
-    
+
+    string[] d1 = { "hello!", "I am your friend.", "How are you?", "This is end of the day progress.", ":)"};
+
     TextManager textManager;
+    NPCdialogueManager npcDialogueManager;
     // Start is called before the first frame update
     void Start()
     {
+        npcDialogueManager = FindObjectOfType<NPCdialogueManager>();
         textManager = FindObjectOfType<TextManager>();
         //StartCoroutine(NarrativeManager());
     }
@@ -28,6 +32,24 @@ public class GameManager : MonoBehaviour
             StartCoroutine(NarrativeManager(SceneTwo));
         }
 
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            StartCoroutine(NPCdialogue(d1));
+        }
+
+    }
+
+    IEnumerator NPCdialogue(string[] dialogue)
+    {
+        int i = 0;
+        while (i < dialogue.Length)
+        {
+
+            npcDialogueManager.AddDialogue(dialogue[i]);
+            i++;
+            while (!npcDialogueManager.isButtonClick) yield return null;
+
+        }
     }
 
     IEnumerator NarrativeManager(string[] narrative)
